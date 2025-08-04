6165a769f4bdda53f9bfc5a0f775a7159c2b6a55
AlejandroRamirezHGI <dramirez.hgi@gmail.com>
2025-08-04 16:02:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from rag_engine import ejecutar_rag
 
-app = FastAPI()
+app = FastAPI(title="Agente Llama3 RAG")
 
 class Query(BaseModel):
     pregunta: str
 
 @app.post("/query")
-def consultar(query: Query):
-    return {"respuesta": ejecutar_rag(query.pregunta)}
+def api_query(q: Query):
+    return {"respuesta": ejecutar_rag(q.pregunta)}
