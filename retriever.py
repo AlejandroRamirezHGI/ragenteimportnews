437cbb006c6262b4c472849c036b812778f856c3
AlejandroRamirezHGI <dramirez.hgi@gmail.com>
2025-08-05 09:41:02 -0500

    Update retriever.py

--- retriever.py
+++ retriever.py
@@ -1,7 +1,7 @@
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
-from langchain.embeddings import HuggingFaceEmbeddings 
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
 
